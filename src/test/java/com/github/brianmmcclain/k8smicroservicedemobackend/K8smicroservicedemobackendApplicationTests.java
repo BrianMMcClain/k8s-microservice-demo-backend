f343c6fdf7d1a8f15ae510739c44d8d03e0c4500
Brian McClain <brianmmcclain@gmail.com>
2020-07-22 15:57:49 -0400

    Initial commit

---
+++ src/test/java/com/github/brianmmcclain/k8smicroservicedemobackend/K8smicroservicedemobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.k8smicroservicedemobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8smicroservicedemobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
