be504ba7e1d0b24c8afea9495dca48b67b3bd151
Brian McClain <brianmmcclain@gmail.com>
2020-07-26 17:55:54 -0400

    Add code from existing demo

---
+++ src/main/java/com/github/brianmmcclain/k8smicroservicedemobackend/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.github.brianmmcclain.k8smicroservicedemobackend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
