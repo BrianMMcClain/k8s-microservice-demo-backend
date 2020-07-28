172f5381d4e5446a2cd48c8432c1f4aaaa1a2a85
Brian McClain <brianmmcclain@gmail.com>
2020-07-28 16:50:24 -0400

    Add /info endpoint

--- src/main/java/com/github/brianmmcclain/k8smicroservicedemobackend/ItemController.java
+++ src/main/java/com/github/brianmmcclain/k8smicroservicedemobackend/ItemController.java
@@ -1,14 +1,32 @@
 package com.github.brianmmcclain.k8smicroservicedemobackend;
 
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class ItemController {
+
     private final ItemRepository repository;
+
+    @Autowired
+    Environment env;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     ItemController(ItemRepository repository) {
         this.repository = repository;
@@ -21,7 +39,30 @@ public class ItemController {
 
     @GetMapping("/item/{id}")
     Item getOne(@PathVariable Long id) {
-        return repository.findById(id)
-            .orElseThrow(() -> new RuntimeException());
+        return repository.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
+    @GetMapping("/info")
+    public String getConfig() {
+        Map<String, String> payload = new HashMap<>();
+        String profiles = "[" + String.join(", ", this.env.getActiveProfiles()) + "]";
+        String databaseType = "Unknown";
+        try {
+            databaseType = jdbcTemplate.getDataSource().getConnection().getMetaData().getDatabaseProductName();
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        payload.put("profiles", profiles);
+        payload.put("database", databaseType);
+        
+        try {
+			return new ObjectMapper().writeValueAsString(payload);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+            e.printStackTrace();
+            return "";
+        }
     }
 }
